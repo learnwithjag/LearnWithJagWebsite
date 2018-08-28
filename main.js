5e88773c94f2f2e4c49a5d264883b4d6580b5090
learnwithjag <40838789+learnwithjag@users.noreply.github.com>
2018-08-28 17:20:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,56 @@
 let data;
+let ctr;
 let subject = document.body.id;
-let video  = document.getElementById('video');
+let video = document.getElementById("video");
 
-function preload()
-{
-  data = loadJSON('/LearnWithJagWebsite/links.json');
+function preload() {
+  data = loadJSON("/links.json");
 }
 
-function setup()
-{
+function setup() {
   noCanvas();
-  let unitHeader = document.getElementsByClassName('unitHeader');
-  let lessons = document.getElementsByClassName('lesson');
-  for(let i = 0; i < unitHeader.length; i++)
-  {
-     unitHeader[i].addEventListener('mousedown', dropdown, false);
+  let unitHeader = document.getElementsByClassName("unitHeader");
+  let lessonContainer = document.getElementsByClassName("lesson-container");
+  let lessons = document.getElementsByClassName("lesson");
+  ctr = 0;
+  for (let i = 0; i < unitHeader.length; i++) {
+    let unit = getUnit(i);
+    console.log("Unit " + (i + 1) + ": " + unit.length);
+    unitHeader[i].addEventListener("mousedown", dropdown, false);
+    for (let j = 0; j < unit.length; j++) {
+      let node = document.createElement("A");
+      let textNode = document.createTextNode(unit[j].name);
+      node.appendChild(textNode);
+      node.classList.add("lesson");
+      node.id = "lesson" + ctr;
+      node.addEventListener("mousedown", change, false);
+      lessonContainer[i].appendChild(node);
+      ctr++;
+    }
   }
-  for(let i = 0; i < lessons.length; i++)
-  {
-     lessons[i].addEventListener('mousedown', change, false);
-  }
-  
-  console.log(lessons.length);
 }
 
-function dropdown()
-{
+function getUnit(i) {
+  return data.subjects[subject].filter(obj => {
+    return obj.unit === i + 1;
+  });
+}
+
+function dropdown() {
   let next = this.nextElementSibling;
-  if(next.style.display === "block")
-    next.style.display = "none";
-  else
-    next.style.display = "block";
+  if (next.style.display === "block") next.style.display = "none";
+  else next.style.display = "block";
 }
 
-function change(event)
-{
+function change(event) {
   let id = event.target.id;
   let lessonNum = parseInt(id.substring(6, id.length));
-  
-  video.src = "https://www.youtube.com/embed/" + data.subjects[subject][lessonNum].videoLink;
-  notes.src = "https://docs.google.com/document/d/e/2PACX-" + data.subjects[subject][lessonNum].docLink + "/pub?embedded=true";
+
+  video.src =
+    "https://www.youtube.com/embed/" +
+    data.subjects[subject][lessonNum].videoLink;
+  notes.src =
+    "https://docs.google.com/document/d/e/2PACX-" +
+    data.subjects[subject][lessonNum].docLink +
+    "/pub?embedded=true";
 }
